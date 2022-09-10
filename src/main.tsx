53a7f783bbbd8374bb16afba4228b04ba62a7d89
Alla <alla@redcollar.ru>
2022-09-11 02:33:44 +0300

    fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.VITE_APP_BASE_PATH as string}>
+    <HashRouter basename={import.meta.env.VITE_APP_BASE_PATH as string}>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
